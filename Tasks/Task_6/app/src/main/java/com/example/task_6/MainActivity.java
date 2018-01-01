396ede2a25fc4d5131ae7e8e2b02d24463aec868
Makar <zlysenko.makarz@gmail.com>
2018-01-01 07:25:31 +0300

    6, 7

---
+++ Tasks/Task_6/app/src/main/java/com/example/task_6/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.task_6;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.rot);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animation);
+            }
+        });
+    }
+}
